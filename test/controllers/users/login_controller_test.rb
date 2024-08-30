c67e0774c83f9afa0e7fe71ff3d39fac4bbc05de
TotalZero <radek.solekr@icloud.com>
2024-08-30 18:22:44 +0200

    added tests to login controller

--- test/controllers/users/login_controller_test.rb
+++ test/controllers/users/login_controller_test.rb
@@ -1,18 +1,17 @@
 require "test_helper"
 
 class Users::LoginControllerTest < ActionDispatch::IntegrationTest
+
+
   test "should get new" do
-    get users_login_new_url
+    get users_login_url
     assert_response :success
+    assert_template :new
   end
 
-  test "should get create" do
-    get users_login_create_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get users_login_destroy_url
-    assert_response :success
-  end
+test "when user written wrong login parameters should be alert" do
+post users_login_url, params: {user: {email: "dotnet@domainet.com", password: "pas"}}
+assert_template :new
+assert_equal "wrong login parameters", flash[:alert]
+end
 end
